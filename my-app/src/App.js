076dc6a7f6e5fe1e6c27970dc9fd66495ffe18bb
Joey Thaman <jthaman@stanford.edu>
2020-09-17 12:46:09 -0400

    finished first design for enter name page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,27 +3,34 @@ import './App.css';
 import './EnterName.js';
 import EnterName from './EnterName.js';
 
+let STATE = {
+    ENTERNAME: 1,
+    ENTEREDUCATION: 2
+}
 
 class App extends React.Component {
     constructor(props) {
-      super(props)
-        this.state = {upvoted:true}
-        this.handleUpvoted = this.handleUpvoted.bind(this)
-        this.handleRecent = this.handleRecent.bind(this)
+        super(props)
+        this.handleNameChange = this.handleNameChange.bind(this)
+        this.handleNameSubmit = this.handleNameSubmit.bind(this)
+
+        this.state = {name:"", currentState: STATE.ENTERNAME}
     }
 
-    handleUpvoted() {
-        this.setState({upvoted:true})
+    handleNameChange(event) {
+        console.log(event.target)
+        this.setState({name:event.target.value})
     }
 
-    handleRecent() {
-        this.setState({upvoted:false})
+    handleNameSubmit() {
+        console.log("bruh")
+        this.setState({currentState:STATE.ENTEREDUCATION})
     }
 
     render() {
         console.log(this.state)
         return (
-            <EnterName/>
+            <EnterName name={this.state.name} handleNameChange={this.handleNameChange} handleNameSubmit={this.handleNameSubmit}/>
         );
     }
 
--- my-app/src/EnterName.js
+++ my-app/src/EnterName.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import './EnterName.css';
+import styled from 'styled-components'
 
+const Button = styled.a`
+  display: inline-block;
+  border-radius: 9px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 7rem;
+  background: transparent;
+  border: 2px solid;
+
+  background: ${props => props.disabled ? "white" : "white"};
+  color: ${props => props.disabled ? "gray" : "palevioletred"};
+  cursor: ${props => props.disabled ? "auto" : "pointer"};
+
+  ${props => props.disabled ? "" :
+    `&:hover {
+        color: red;
+    }`
+  }
+`
 
 class EnterName extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {upvoted:true}
-        this.handleUpvoted = this.handleUpvoted.bind(this)
-        this.handleRecent = this.handleRecent.bind(this)
-    }
-
-    handleUpvoted() {
-        this.setState({upvoted:true})
-    }
-
-    handleRecent() {
-        this.setState({upvoted:false})
     }
 
     render() {
@@ -23,12 +32,20 @@ class EnterName extends React.Component {
         return (
             <div className="EnterName">
                 <div className="col-sm-12">
-                    EnterNamePage
+                    <h2 style={{textAlign:"center"}}>Hi there! Welcome to your education showcase</h2>
+                    <h2 style={{textAlign:"center"}}>Type your name and click "Enter" below to begin!</h2>
+                    <div style={{textAlign:"center"}}>
+                        <input type="text" placeholder="Your name" value={this.props.name} onChange={e => this.props.handleNameChange(e)}></input>
+                    </div>
+                    <div style={{textAlign:"center"}}>
+                        <Button disabled={this.props.name==""} onClick={this.props.handleNameSubmit}>
+                            Enter
+                        </Button>
+                    </div>
                 </div>
             </div>
         );
     }
-
 }
 
 export default EnterName;
